fe0f227aadc0424b9c68260079b3eaa01e7fa13d
Francois Gelinas <frank@fgelinas.com>
2011-06-16 21:29:14 -0400

    0.2.2 - Fixed a "console.log" bug with previous commit

--- jquery.ui.timepicker.js
+++ jquery.ui.timepicker.js
@@ -1,5 +1,5 @@
 /*
- * jQuery UI Timepicker 0.2.1
+ * jQuery UI Timepicker 0.2.2
  *
  * Copyright 2010-2011, Francois Gelinas
  * Dual licensed under the MIT or GPL Version 2 licenses.
@@ -40,7 +40,7 @@
 
 (function ($, undefined) {
 
-    $.extend($.ui, { timepicker: { version: "0.2.1"} });
+    $.extend($.ui, { timepicker: { version: "0.2.2"} });
 
     var PROP_NAME = 'timepicker';
     var tpuuid = new Date().getTime();
@@ -515,10 +515,6 @@
             }
             for (m = minutes_options.starts; m <= minutes_options.ends; m += minutes_options.interval) {
                 minutes.push(m);
-            }
-            if (minutes_options.interval == 15) {
-                console.log('found it');
-                console.log(minutes_options.starts);
             }
             minutesPerRow = Math.round(minutes.length / rows + 0.49); // always round up
 
@@ -1012,7 +1008,7 @@
     $.timepicker = new Timepicker(); // singleton instance
     $.timepicker.initialized = false;
     $.timepicker.uuid = new Date().getTime();
-    $.timepicker.version = "0.2.1";
+    $.timepicker.version = "0.2.2";
 
     // Workaround for #4055
     // Add another global to avoid noConflict issues with inline event handlers

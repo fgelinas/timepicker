8d0744d26e760e83b47b5f6d9a563f09103131fa
Francois Gelinas <frank@fgelinas.com>
2011-10-13 21:42:47 -0400

    Release 0.2.6

--- jquery.ui.timepicker.js
+++ jquery.ui.timepicker.js
@@ -1,5 +1,5 @@
 /*
- * jQuery UI Timepicker 0.2.5
+ * jQuery UI Timepicker 0.2.6
  *
  * Copyright 2010-2011, Francois Gelinas
  * Dual licensed under the MIT or GPL Version 2 licenses.
@@ -40,7 +40,7 @@
 
 (function ($, undefined) {
 
-    $.extend($.ui, { timepicker: { version: "0.2.5"} });
+    $.extend($.ui, { timepicker: { version: "0.2.6"} });
 
     var PROP_NAME = 'timepicker';
     var tpuuid = new Date().getTime();
@@ -1283,7 +1283,7 @@
     $.timepicker = new Timepicker(); // singleton instance
     $.timepicker.initialized = false;
     $.timepicker.uuid = new Date().getTime();
-    $.timepicker.version = "0.2.5";
+    $.timepicker.version = "0.2.6";
 
     // Workaround for #4055
     // Add another global to avoid noConflict issues with inline event handlers

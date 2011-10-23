b4fb8ea4f4234c6ea8c562fecfed151261283be5
Blaž Maležič <blaz@malezic.si>
2011-10-23 11:05:31 +0200

    Added new localization file - Slovenian

---
+++ i18n/jquery.ui.timepicker-sl.js
@@ -0,0 +1,13 @@
+/* Slovenian localization for the jQuery time picker plugin. */
+/* Written by Bernd Plagge (bplagge@choicenet.ne.jp),
+              Francois Gelinas (frank@fgelinas.com) */
+jQuery(function($){
+    $.timepicker.regional['sl'] = {
+                hourText: 'Ure',
+                minuteText: 'Minute',
+                amPmText: ['AM', 'PM'],
+                closeButtonText: 'Zapri',
+                nowButtonText: 'Zdaj',
+                deselectButtonText: 'Pobriši' }
+    $.timepicker.setDefaults($.timepicker.regional['sl']);
+});
